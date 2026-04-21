1397117b991c08baff4d60ae8e505f13fc028584
Matthew Mason <m.mason29272@gmail.com>
2026-04-21 04:45:17 +0100

    updated firsebase

--- firebase.js
+++ firebase.js
@@ -4,9 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyC2FgxC1ykjt8o26ofl1HavzqD1rHQMJnY",
+  authDomain: "dabackend-e72f7.firebaseapp.com",
+  projectId: "dabackend-e72f7",
+  storageBucket: "dabackend-e72f7.firebasestorage.app",
+  messagingSenderId: "892294310337",
+  appId: "1:892294310337:web:3c4ab150998847fe242740",
+  measurementId: "G-3DEQ13YWTG"
 };
 
 const app = initializeApp(firebaseConfig);
